bbf757403da4bd8f81d26788ad695cdccefb371e
Unknown <jackrgoetz@gmail.com>
2018-03-14 13:13:26 -0400

    Testing MT vs BT with chem data

--- compare_with_BT.py
+++ compare_with_BT.py
@@ -20,23 +20,45 @@ for i in range(X.shape[1]):
 
 y = data[:,-1]
 
-n, p = X.shape
+train_test_seed = 1
+
+cv_ind = np.random.permutation(range(X.shape[0]))
+
+train_ind = cv_ind[:1000]
+test_ind = cv_ind[1000:]
+
+X_train = X[train_ind,:]
+X_test = X[test_ind,:]
+
+y_train = y[train_ind]
+y_test = y[test_ind]
+
+n, p = X_train.shape
 print(n,p)
 
-seed = 1
+seed = 4
 
 MT = Mondrian_Tree([[0,1]]*p)
 MT.update_life_time(n**(1/(2+p))-1, set_seed=seed)
-print(MT._num_leaves)
-MT.input_data(X, range(n), y)
+# print(MT._num_leaves)
+MT.input_data(X_train, range(1000), y_train)
 
-MT_preds = MT.predict(X)
+MT.make_full_leaf_list()
+used_leaf_counter = 0
+for node in MT._full_leaf_list:
+    if len(node.labelled_index) != 0:
+        print(len(node.labelled_index), node.calculate_cell_l2_diameter())
+        used_leaf_counter += 1
+print(used_leaf_counter)
+
+MT.set_default_pred_global_mean()
+MT_preds = MT.predict(X_test)
 MT_preds = np.array(MT_preds)
 
-print(sum(1/n*(y - MT_preds)**2))
+print(sum(1/n*(y_test - MT_preds)**2))
 
-BT = DecisionTreeRegressor(random_state=seed, max_leaf_nodes = MT._num_leaves)
-BT.fit(X, y)
-BT_preds = BT.predict(X)
+BT = DecisionTreeRegressor(random_state=seed, max_leaf_nodes = used_leaf_counter)
+BT.fit(X_train, y_train)
+BT_preds = BT.predict(X_test)
 
-print(sum(1/n*(y - BT_preds)**2))
+print(sum(1/n*(y_test - BT_preds)**2))
