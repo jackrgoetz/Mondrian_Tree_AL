f891cd9040a397cee72c8a39d779a5e45f59eb6e
Unknown <jackrgoetz@gmail.com>
2018-03-19 17:44:08 -0400

    Track simulation data creating files

---
+++ data_sets/toy_data_pw_linear.py
@@ -0,0 +1,32 @@
+# Create toy data with constant variance but changing OLS slope (same slope value for all dim)
+
+import random
+
+def toy_data_pw_linear(n,p,high_area,high_constant=3, low_constant=1,std=1, set_seed=None):
+
+    if set_seed is not None:
+        random.seed(set_seed)
+
+    points = []
+    labels = []
+    for i in range(n):
+        point = []
+        is_high_var = []
+        for j in range(p):
+            val = random.random()
+            point.append(val)
+            if val > high_area[j][0] and val < high_area[j][1]:
+                is_high_var.append(True)
+            else:
+                is_high_var.append(False)
+
+        if all(is_high_var):
+            labels.append(high_constant*sum(point) + random.gauss(0, std))
+        else:
+            labels.append(low_constant*sum(point) + random.gauss(0, std))
+        points.append(point)
+
+
+    return points, labels
+
+
---
+++ data_sets/toy_data_var.py
@@ -0,0 +1,32 @@
+# Create toy data with constan mean but heteroskedasticity
+
+import random
+
+def toy_data_var(n,p,high_area,constant=0,low_std=1,high_std=3, set_seed=None):
+
+    if set_seed is not None:
+        random.seed(set_seed)
+
+    points = []
+    labels = []
+    for i in range(n):
+        point = []
+        is_high_var = []
+        for j in range(p):
+            val = random.random()
+            point.append(val)
+            if val > high_area[j][0] and val < high_area[j][1]:
+                is_high_var.append(True)
+            else:
+                is_high_var.append(False)
+
+        if all(is_high_var):
+            labels.append(constant + random.gauss(0, high_std))
+        else:
+            labels.append(constant + random.gauss(0, low_std))
+        points.append(point)
+
+
+    return points, labels
+
+
