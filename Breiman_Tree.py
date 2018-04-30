68c3da07d1d5d81bb11993e798a899d2796bfb07
Unknown <jackrgoetz@gmail.com>
2018-04-30 17:03:17 -0400

    Making Breiman Tree and testing

--- Breiman_Tree.py
+++ Breiman_Tree.py
@@ -1,9 +1,12 @@
 from sklearn.tree import DecisionTreeRegressor
+from collections import Counter
 
+import utils
 import numpy as np
 import matplotlib
 matplotlib.use('AGG')
 import matplotlib.pyplot as plt
+import copy
 
 class Breiman_Tree:
 
@@ -11,11 +14,11 @@ class Breiman_Tree:
     Main class for Breiman Tree version of active learning algorithm
     '''
 
-    def __init__(self, seed=None):
+    def __init__(self, min_samples_leaf=None, seed=None):
 
         self.points = None
         self.labels = None
-        self.labelled_index = None
+        self.labelled_indices = None
         self._num_points = 0
         self._num_labelled = 0
 
@@ -23,3 +26,90 @@ class Breiman_Tree:
             self.seed = 0
         else:
             self.seed = seed
+
+        if min_samples_leaf is None:
+            self.min_samples_leaf=1
+        else:
+            self.min_samples_leaf=min_samples_leaf
+
+        self.tree = DecisionTreeRegressor(random_state=self.seed,min_samples_leaf=self.min_samples_leaf)
+        self._leaf_indices = []
+        self._leaf_marginal = []
+        self._leaf_var = []
+        self._al_proportions =[]
+
+        self._leaf_statistics_up_to_date = False
+
+        self._verbose = False
+
+    def input_data(self, all_data, labelled_indices, labels, copy_data=True):
+
+        if copy_data:
+            all_data = copy.deepcopy(all_data)
+            labelled_indices = copy.deepcopy(labelled_indices)
+            labels = copy.deepcopy(labels)
+
+        if len(all_data) < len(labelled_indices):
+            raise ValueError('Cannot have more labelled indicies than points')
+
+        if len(labelled_indices) != len(labels):
+            raise ValueError('Labelled indicies list and labels list must be same length')
+
+        if str(type(all_data)) == "<class 'numpy.ndarray'>":
+            if self._verbose:
+                print('Converting all_data to list of lists internally')
+            all_data = all_data.tolist()
+
+        if str(type(labelled_indices)) == "<class 'numpy.ndarray'>":
+            if self._verbose:
+                print('Converting labelled_indices to list internally')
+            labelled_indices = labelled_indices.tolist()
+
+        if str(type(labels)) == "<class 'numpy.ndarray'>":
+            if self._verbose:
+                print('Converting labels to list internally')
+            labels = labels.tolist()
+
+        self.points = all_data
+        self._num_points = len(self.points)
+        self._num_labelled = len(labels)
+
+        # Making a label list, with None in places where we don't have the label
+
+        temp = [None] * self._num_points
+        for i,ind in enumerate(labelled_indices):
+            temp[ind] = labels[i]
+        self.labels = temp
+        self.labelled_indices = list(labelled_indices)
+
+    def fit_tree(self):
+        self.tree.fit(np.array(self.points)[self.labelled_indices,:], 
+            np.array(self.labels)[self.labelled_indices])
+        self._leaf_indices = self.tree.apply(np.array(self.points))
+        self._leaf_statistics_up_to_date = False
+
+    def label_point(self, index, value):
+
+        if self.labels is None:
+            raise RuntimeError('No data in the tree')
+
+        if len(self.labels) <= index:
+            raise ValueError('Index {} larger than size of data in tree'.format(index))
+
+        value = copy.copy(value)
+        index = copy.copy(index)
+
+        self.labels[index] = value
+        self.labelled_indices.append(index)
+
+    def calculate_leaf_statistics(self):
+        temp = Counter(self._leaf_indices)
+        self._leaf_marginal = []
+        self._leaf_var = []
+        for key in np.unique(self._leaf_indices):
+            self._leaf_marginal.append(temp[key]/self._num_points)
+            temp_ind = [i for i,x in enumerate(self._leaf_indices) if x == key]
+            temp_labels = [x for x in self.labels if x is not None]
+            self._leaf_var.append(utils.unbiased_var(temp_labels))
+
+    def al_calculate_leaf_proportions(self):
---
+++ test_Breiman_Tree.py
@@ -0,0 +1,70 @@
+import unittest
+import random
+
+from Breiman_Tree import Breiman_Tree
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+
+class test_Breiman_Tree(unittest.TestCase):
+
+    def setUp(self):
+
+        self.d = 10
+        self.a = 1
+        self.bt = Breiman_Tree(min_samples_leaf=5)
+
+        self.n_points = 100
+        self.n_labelled = 20
+        random.seed(123)
+        self.labels = [random.random() for i in range(self.n_labelled)]
+        self.labelled_indices = range(self.n_labelled)
+        self.data = []
+        random.seed(1)
+        for i in range(self.n_points):
+            point = []
+            for j in range(self.d):
+                point.append(random.random())
+            self.data.append(point)
+
+    def test_building_tree_with_same_data(self):
+        n_reps = 20
+        self.bt.input_data(self.data, self.labelled_indices, self.labels)
+        tree_lens = []
+        for _ in range(n_reps):
+            self.bt.fit_tree()
+            tree_lens.append(self.bt.tree.tree_.node_count)
+            # print(self.bt.tree.tree_.node_count)
+        self.assertEqual([tree_lens[0]]*n_reps, tree_lens)
+
+    def test_building_tree_with_incremental_data(self):
+        '''No actual tests just printing things to look at how Breiman trees behave'''
+        n_reps = 20
+        self.bt.input_data(self.data, self.labelled_indices, self.labels)
+        tree_lens = []
+        for i in range(n_reps):
+            self.bt.label_point(self.n_labelled+i, random.random())
+            self.bt.fit_tree()
+            # print(np.unique(self.bt.tree.tree_.threshold))
+            tree_lens.append(len([x for x in self.bt.tree.tree_.children_left if x == -1]))
+            
+            # print(self.bt.tree.tree_.node_count)
+        # print(tree_lens)
+
+    def test_leaf_statistics_var(self):
+        n_reps = 20
+        self.bt.input_data(self.data, self.labelled_indices, self.labels)
+        for i in range(n_reps):
+            self.bt.label_point(self.n_labelled+i, random.random())
+            self.bt.fit_tree()
+            self.bt.calculate_leaf_statistics()
+            print(sorted(self.bt._leaf_var))
+
+    def test_leaf_statistics_marginal(self):
+        n_reps = 20
+        self.bt.input_data(self.data, self.labelled_indices, self.labels)
+        for i in range(n_reps):
+            self.bt.label_point(self.n_labelled+i, random.random())
+            self.bt.fit_tree()
+            self.bt.calculate_leaf_statistics()
+            print(sorted(self.bt._leaf_marginal))
+        
